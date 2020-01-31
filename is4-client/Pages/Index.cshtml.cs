43f7b148b4d7b565b4a23ef19043d6c14ad8f175
Paul Campbell <paul.campbell@myunidays.com>
2020-01-31 16:14:59 +0000

    initial commit

---
+++ is4-client/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace is4_client.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
